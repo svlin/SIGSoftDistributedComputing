ce3609405be6b1c3e1a3af0a01b62fbe25c936df
Sophia <sophia.vivie.lin@gmail.com>
2014-11-25 17:27:28 -0600

    JTextField only accepts integers. Program shows error if value entered is null or 0.

--- DCApp.java
+++ DCApp.java
@@ -1,20 +1,24 @@
 /**
- * Created by Stephen, Andrew, and Sophia on 10/11/14.
+ * Created by Stephen S. and Sophia L. on 10/11/14.
  */
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class DCApp implements ActionListener {
-    JTextField runsBox;
-    JTextArea log;
+    JTextField runsBox = new JTextField(40);
+    JTextArea log = new JTextArea(10, 10);
+    JButton button = new JButton("Click Me!");
+    JFrame frame = new JFrame("Calculating Pi");
+
     static DCApp app;
     String str;
     int val_str;
-    //boolean letter;
-    
+
     public static void main(String[] args) {
         app = new DCApp();
         app.init();
@@ -22,23 +26,31 @@ public class DCApp implements ActionListener {
     
     public void init() {
         DCApp gui = new DCApp();
-        
-        JFrame frame = new JFrame("Calculating Pi");
+
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JButton button = new JButton("Click Me!");
+
         button.addActionListener(gui);
-        runsBox = new JTextField(40);
+
+        runsBox.addKeyListener(new KeyAdapter() {
+            public void keyTyped (KeyEvent e) {
+                char c = e.getKeyChar();
+
+                if (!(Character.isDigit(c)) || (c == KeyEvent.VK_BACK_SPACE) || (c == KeyEvent.VK_DELETE))
+                {
+                    e.consume();
+                }
+            }
+        });
         
         frame.getContentPane().setLayout(new BorderLayout());
         frame.getContentPane().add(button, BorderLayout.NORTH);
         
-        JPanel p1 = new JPanel();
-        frame.getContentPane().add(p1);
-        p1.add(new JLabel("Runs: "));
-        p1.add(runsBox);
-        
-        log = new JTextArea(10, 10);
+        JPanel panel = new JPanel();
+        frame.getContentPane().add(panel);
+        panel.add(new JLabel("Runs: "));
+        panel.add(runsBox);
+
         log.setEditable(false);
         
         frame.getContentPane().add(log, BorderLayout.CENTER);
@@ -46,24 +58,23 @@ public class DCApp implements ActionListener {
         
         frame.setSize(600, 600);
     }
-    
+
     public void actionPerformed(ActionEvent evt) {
         str = app.runsBox.getText();
 
         if (str.length() != 0)
         {
             val_str = Integer.parseInt(str);
-            //letter = str.matches(".*[a-zA-Z]+.*");
         }
-        
+
         try {
-            if (!(str.equals("") || val_str <= 0)) {
+            if (!(str.equals("") || val_str == 0)) {
                 System.out.println(str);
                 MonteCarloSim sim = new MonteCarloSim(Integer.parseInt(str.trim()));
                 sim.runAll();
                 long hits = sim.getHits();
                 System.out.println(hits);
-                
+
                 Connection connection = new Connection("http://tensile-tenure-727.appspot.com/totals");
                 connection.post(hits, Integer.parseInt(str.trim()));
             } else {
